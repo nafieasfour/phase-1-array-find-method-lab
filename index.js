ebd0afae975f7faa69dbf2fcebf9aebc3c2b3154
Nafie Asfour <ph.nafee.asfour@gmail.com>
2023-06-13 11:05:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+
+// function superbowlWin(array){
+//     array.find(didWin)
+//     function didWin(array){
+//         return array['year']
+//         function didWin(array){
+//             (array['result'] == "W")
+//     }}}
+function superbowlWin(array) {
+    const win = array.find(obj => obj.result === "W");
+    return win ? win.year : undefined;
+  }
+  
